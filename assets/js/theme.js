4c236e4445c183f5afeb465f0b421b08f4e8502e
Inese Krole <inese.krole@gmail.com>
2023-05-02 01:05:06 +0300

    some adjustments, fixing bugs

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,22 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".toggle-btn");
   const toggleBtnIcon = document.querySelector(".toggle-btn i");
+
   const dropDownMenu = document.querySelector(".dropdown-menu");
+
+  const toggleBtnTablet = document.querySelector(".toggle-btn-tablet");
+  const toggleBtnIconTablet = document.querySelector(".toggle-btn-tablet i");
+
+  function toggleMenuIcon() {
+    if (window.innerWidth >= 320) {
+      if (dropDownMenu.classList.contains("open")) {
+        toggleBtnIconTablet.classList.replace("fa-bars", "fa-xmark");
+      } else {
+        toggleBtnIconTablet.classList.replace("fa-xmark", "fa-bars");
+      }
+      toggleBtnTablet.style.display = "block";
+      toggleBtn.style.display = "none";
+    } else {
+      toggleBtnIcon.classList.replace("fa-bars", "fa-xmark");
+      toggleBtn.style.display = "block";
+      toggleBtnTablet.style.display = "none";
+    }
+  }
+
+  toggleMenuIcon();
+
+  window.addEventListener("resize", toggleMenuIcon);
 
   toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle("open");
+    toggleMenuIcon();
+  };
+
+  toggleBtnTablet.onclick = function () {
+    dropDownMenu.classList.toggle("open");
+    toggleMenuIcon();
   };
 
   const dropdownItems = document.querySelectorAll(".dropdown-menu ul li");
 
   dropdownItems.forEach((item) => {
     item.addEventListener("click", () => {
-      // Remove the "active" class from the currently active link, if there is one
       const activeItem = document.querySelector(".dropdown-menu ul li.active");
       if (activeItem) {
         activeItem.classList.remove("active");
       }
-      // Add the "active" class to the clicked <a> element
+
       item.classList.add("active");
     });
   });
